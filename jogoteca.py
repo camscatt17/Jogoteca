79eae11cab608b235a85c2354d7645ff1553854d
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-05-10 08:53:36 -0300

    Utilizando orientação a objetos e Jinja2 com Flask.

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
 @app.route('/inicio')
-def ola():
-    return render_template('lista.html')
+def inicio():
+    tetris = Jogo('Tetris', 'Puzzle', 'Atari')
+    godOfWar = Jogo('God of War', 'Rack n Slash', 'PS2')
+    mortalkombat = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [tetris, godOfWar, mortalkombat]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
